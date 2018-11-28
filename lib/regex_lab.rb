4cf21934f870ed961253f153138d24f5f1338840
Nicholas Rozmus <rozmusnicholas@gmail.com>
2018-11-28 01:17:57 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  "afoot Excellent incredible Absolute unreal Inconceivable octopus".match(/\w+aeiou/)
+  word = "afoot Excellent incredible Absolute unreal Inconceivable octopus".match(/\w+aeiou/)
 
 end
 
